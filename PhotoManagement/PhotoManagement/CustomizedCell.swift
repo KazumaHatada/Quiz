0c83069d719986356598f7e91659a35ea6af17c7
Kazuma Hatada <kazumahatada@KazumanoMacBook-ea.local>
2019-08-19 12:14:39 +0900

    EXIF情報を取得するアプリを追加

---
+++ PhotoManagement/PhotoManagement/CustomizedCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomizedCell.swift
+//  PhotoManagement
+//
+//  Created by Kazuma Hatada on 2019/08/05.
+//  Copyright © 2019 Kazuma Hatada. All rights reserved.
+//
+
+import UIKit
+
+class CustomizedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
